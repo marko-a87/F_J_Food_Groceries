60e43744fce812109f9fdc5274acd5d769647253
marko-a87 <malco0253@gmail.com>
2023-11-02 20:56:07 -0500

    Added order controllers

---
+++ backend/controllers/orderController.js
@@ -0,0 +1,9 @@
+import express from "express";
+
+const placeOrder = (req, res) => {};
+
+const orderDelivery = (req, res) => {};
+
+const orderPickup = (req, res) => {};
+
+export { placeOrder, orderDelivery, orderPickup };
