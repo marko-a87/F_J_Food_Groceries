36ef27e33c42417cebfe14aade973df536162b75
marko-a87 <malco0253@gmail.com>
2023-11-25 15:51:45 -0500

    Worked on Authentication code

--- django_v/F_J_app/Authentication/Customer.py
+++ django_v/F_J_app/Authentication/Customer.py
@@ -1,25 +1,41 @@
 # Creates a customer for the database to store
+import os
+import sys
+
+# Get the directory of the current script
+script_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Append the parent directory to sys.path
+sys.path.append(os.path.dirname(script_dir))
+from Authentication.System_UI import System_UI
 
 
 class Customer:
     # initializes all class variables
-    def __init__(self, name: str, password: str, email_address: str):
-        self.name = name
+    def __init__(self, user_name: str, password: str, email_address: str):
+        self.user_name = user_name
         self.password = password
         self.email_address = email_address
 
-    def register():
+    def __init__(self):
+        """Empty constructor"""
+
+    def register(self):
         """Method registers customer"""
         print("User registered")
-        name: str = input("")
-        password: str = input("")
-        email_address: str = input("")
-        customer_type = Customer(name, password, email_address)
+        self.user_name: str = input("Enter the username: ")
+        self.password: str = input("Enter the password: ")
+        self.email_address: str = input("Enter the email address: ")
+        """if username, password and email_address not in database then, create a customer instance"""
+        """Otherwise say that """
+
+        """If username exists already throw a username error"""
+        """If password exists already throw a password error"""
+        """If email exists already throw an email error"""
 
     # Checks user credentials against database
-    def login():
+    def login(self):
         """If records from customer_type matches database then login"""
-        if customer_type == True:
-            print("Logged in")
-        else:
-            print("Invalid credentials")
+        print("Logged in")
+        System_UI.verifyLogin(self.user_name, self.password)
+        """Otherwise throw a Invalid credentials error"""
--- django_v/F_J_app/Authentication/System_UI.py
+++ django_v/F_J_app/Authentication/System_UI.py
@@ -1,4 +1,13 @@
 # System_UI verifies the login information of each customer class
+import os
+import sys
+
+# Get the directory of the current script
+script_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Append the parent directory to sys.path
+sys.path.append(os.path.dirname(script_dir))
+from Authentication.Customer import Customer
 
 
 class System_UI:
@@ -6,6 +15,17 @@ class System_UI:
         self.status = status
 
     # Verfies customer information based on whats stored in database
-    def verifyLogin(self):
+    def verifyLogin(self, username: str, password: str):
         # You can implement the code to verify customer login information here
+        """If login status is successful then return true"""
         print("Login successful")
+        """Otherwise return false"""
+
+
+client = Customer()  # Creates customer instance
+client.register()  # Register customer instance
+client.login(client.user_name, client.password)  # login customer
+"""if login is valid then set System_UI status to true"""
+
+
+print(client.user_name)
