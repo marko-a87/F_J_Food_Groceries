704d1d25cae38901bdf862a1f1797c43f00ada6b
JemmiJ <145737577+JemmiJ@users.noreply.github.com>
2023-11-26 15:50:53 -0500

    Update models.py

--- django_v/F_J_app/Database/models.py
+++ django_v/F_J_app/Database/models.py
@@ -3,28 +3,30 @@ from django.db import models
 # Create models here
 
 class Customer(models.Model):
+    name = models.CharField(max_length=50)
+    age = models.PositiveIntegerField(int)
     username = models.CharField(max_length=50)
     password = models.CharField(max_length=50)
     email_address = models.CharField(max_length=100)
+    def __str__(self):
+        return self.username
 
 class Product(models.Model):
     name=  models.CharField(max_length=50)
     description = models.CharField(max_length=100)
-    cost = 
+    cost = models.FloatField()
+    def __str__(self):
+        return self.name
 
 class Delivery(models.Model):
-    name = models.CharField(max_length=50)
-    age = models.PositiveIntegerField(int)
     email_address = models.EmailField(True)
     delivery_address= models.CharField(max_length=80)
     phone_number = models.CharField(max_length=20)
 
 
 class Order(models.Model):
-    order_number = 
-    date_ordered = 
-    date_shipped = 
-    status:  =
-    customer_name =
-
-class Cart(models.Model):
+    order_number = models.IntegerField(int)
+    date_ordered = models.DateField()
+    date_shipped = models.DateField
+    status  = models.BooleanField()
+    customer_name =models.ForeignKey(Delivery, on_delete=models.CASCADE)
