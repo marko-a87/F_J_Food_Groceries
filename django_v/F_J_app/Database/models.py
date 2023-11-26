d58167c7cf75ec98df3c3644cdc452be8295961a
marko-a87 <malco0253@gmail.com>
2023-11-26 02:37:20 -0500

    Updated models

--- django_v/F_J_app/Database/models.py
+++ django_v/F_J_app/Database/models.py
@@ -2,29 +2,33 @@ from django.db import models
 
 # Create models here
 
+
 class Customer(models.Model):
     username = models.CharField(max_length=50)
     password = models.CharField(max_length=50)
     email_address = models.CharField(max_length=100)
 
+
 class Product(models.Model):
-    name=  models.CharField(max_length=50)
+    name = models.CharField(max_length=50)
     description = models.CharField(max_length=100)
-    cost = 
+    cost = models.IntegerField()
+
 
 class Delivery(models.Model):
     name = models.CharField(max_length=50)
     age = models.PositiveIntegerField(int)
     email_address = models.EmailField(True)
-    delivery_address= models.CharField(max_length=80)
+    delivery_address = models.CharField(max_length=80)
     phone_number = models.CharField(max_length=20)
 
 
 class Order(models.Model):
-    order_number = 
-    date_ordered = 
-    date_shipped = 
-    status:  =
-    customer_name =
+    order_number = models.IntegerField()
+    date_ordered = models.DateTimeField()
+    date_shipped = models.DurationField()
+    status = models.BooleanField()
+    customer_name = models.CharField(max_length=100)
 
-class Cart(models.Model):
+
+# class Cart(models.Model):
