ed298f30c847a1aadf4ea8e889e38a9d0841235f
JemmiJ <145737577+JemmiJ@users.noreply.github.com>
2023-11-27 20:07:36 -0500

    Update models.py

--- django_v/F_J_app/Database/models.py
+++ django_v/F_J_app/Database/models.py
@@ -1,7 +1,6 @@
 from django.db import models
-
+from django.urls import reverse
 # Create models here
-
 
 class Customer(models.Model):
     name = models.CharField(max_length=50)
@@ -12,25 +11,31 @@ class Customer(models.Model):
     def __str__(self):
         return self.username
 
-
 class Product(models.Model):
-    name = models.CharField(max_length=50)
+    name=  models.CharField(max_length=50)
     description = models.CharField(max_length=100)
-    cost = 
+    cost = models.FloatField()
+    def __str__(self):
+        return self.name
 
 class Delivery(models.Model):
-    name = models.CharField(max_length=50)
-    age = models.PositiveIntegerField(int)
     email_address = models.EmailField(True)
-    delivery_address = models.CharField(max_length=80)
+    delivery_address= models.CharField(max_length=80)
     phone_number = models.CharField(max_length=20)
 
 
 class Order(models.Model):
-    order_number = 
-    date_ordered = 
-    date_shipped = 
-    status:  =
-    customer_name =
+    order_number = models.IntegerField(int)
+    date_ordered = models.DateField()
+    date_shipped = models.DateField
+    status  = models.BooleanField()
+    customer_name =models.ForeignKey(Delivery, on_delete=models.CASCADE)
 
 class Cart(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=0)
+    customer = models.ForeignKey(Customer, on_delete = models.CASCADE)
+    def __str__(self):
+        return f'{self.quantity} + {self.product.name}'
+    def get_absolute_url(self):
+        return reverse("cart:cart_detail")
