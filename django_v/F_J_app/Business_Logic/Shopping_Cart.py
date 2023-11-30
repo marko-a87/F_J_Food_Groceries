53b815e04ea64e4f5dbc3f8e23773c650dbdb123
JemmiJ <145737577+JemmiJ@users.noreply.github.com>
2023-11-30 14:05:38 -0500

    Update Shopping_Cart.py

--- django_v/F_J_app/Business_Logic/Shopping_Cart.py
+++ django_v/F_J_app/Business_Logic/Shopping_Cart.py
@@ -13,32 +13,31 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect, get_object_or_404
 from Database.models import Cart
 
-@login_required
-def cart_view(request):
-    cartlist = Cart.objects.filter(customer = request.customer)
-    totalprice = sum(citem.product.product_cost * citem.quantity for citem in cartlist)
-    holder = {"cart items": cartlist, "totalprice": totalprice }
-    return render(request, "cart/shop_cart.html", holder)
+class Cart_Shop:
+    @login_required
+    def cart_view(request):
+        cartlist = Cart.objects.filter(customer = request.customer)
+        totalprice = sum(citem.product.product_cost * citem.quantity for citem in cartlist)
+        holder = {"cart items": cartlist, "totalprice": totalprice }
+        return render(request, "cart/shop_cart.html", holder)
+    @login_required
+    def add_product_to_cart(request, product_name):
+        cartlist = Cart.objects.filter(customer = request.customer, product=product_name)
+        if cartlist:
+            cartlist.quantity+=1
+            cartlist.save()
+            messages.success(request, "This product is added to your cart")
+        else:
+            Cart.objects.create(customer = request.customer, product=product_name)
+            messages.success(request, "This product is added to your cart")
+        return redirect("cart:cart_view")
+    @login_required
+    def remove_product_from_cart(request, item_id):
+        cartlist = get_object_or_404(Cart, id = item_id)
+    
+        if cartlist.customer == request.customer:
+            cartlist.delete()
+            messages.success(request, "This product has been removed")
+        return redirect("cart:cart_view")
 
-@login_required
-def add_product_to_cart(request, product_name):
-    cartlist = Cart.objects.filter(customer = request.customer, product=product_name)
-    if cartlist:
-        cartlist.quantity+=1
-        cartlist.save()
-        messages.success(request, "This product is added to your cart")
-    else:
-        Cart.objects.create(customer = request.customer, product=product_name)
-        messages.success(request, "This product is added to your cart")
-    return redirect("cart:cart_view")
-
-@login_required
-def remove_product_from_cart(request, item_id):
-    cartlist = get_object_or_404(Cart, id = item_id)
-
-    if cartlist.customer == request.customer:
-        cartlist.delete()
-        messages.success(request, "This product has been removed")
-    return redirect("cart:cart_view")
-
-print(Authentication.Customer.status)
+    print(Authentication.Customer.status)
