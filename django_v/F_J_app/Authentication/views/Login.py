ad19ce1fa7d14c08812b57277ea17a64a0c6a717
marko-a87 <malco0253@gmail.com>
2023-12-01 08:04:37 -0500

    Updated functionality

--- django_v/F_J_app/Authentication/views/Login.py
+++ django_v/F_J_app/Authentication/views/Login.py
@@ -3,6 +3,8 @@ from django.contrib.auth.hashers import check_password
 
 # from store.models.customer import Customer
 from django.views import View
+from Database.models import *
+from Database.Database import *
 
 
 class Login(View):
@@ -14,3 +16,18 @@ class Login(View):
             request,
             "login.html",
         )
+
+
+    def post(self, request):
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+        # Perform login authentication using the Database class or any other authentication mechanism
+        storage = Database()
+        status = storage.login(email, password)
+
+        if status:
+            # Login successful, redirect to the home page or the intended return_url
+            return redirect(Login.return_url or "home")
+        else:
+            # Login failed, you may want to display an error message or redirect to the login page
+            return render(request, "login.html", {"error": "Invalid credentials"})
--- django_v/F_J_app/Authentication/views/Signup.py
+++ django_v/F_J_app/Authentication/views/Signup.py
@@ -3,6 +3,8 @@ from django.contrib.auth.hashers import check_password
 
 # from store.models.customer import Customer
 from django.views import View
+from Database.models import *
+from Database.Database import *
 
 
 class Register(View):
@@ -14,3 +16,27 @@ class Register(View):
             request,
             "register.html",
         )
+
+    def post(self, request):
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        email = request.POST.get("email")
+
+        # Perform registration using the Database class or any other registration mechanism
+        storage = Database()
+        customer = Customer(username=username, password=password, email_address=email)
+        storage.register(customer)
+
+        # Optionally, you can perform login after registration
+        status = storage.login(username, password)
+
+        if status:
+            # Registration and login successful, redirect to the home page or the intended return_url
+            return redirect(Register.return_url or "home")
+        else:
+            # Registration successful but login failed, you may want to display an error message or redirect to the login page
+            return render(
+                request,
+                "login.html",
+                {"error": "Registration successful, but login failed"},
+            )
