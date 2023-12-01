0db82b77d3f2b5a83cd78086b0bbdb99507bbe8f
JemmiJ <145737577+JemmiJ@users.noreply.github.com>
2023-12-01 04:07:23 -0500

    Update Shopping_Cart.py

--- django_v/F_J_app/Business_Logic/Shopping_Cart.py
+++ django_v/F_J_app/Business_Logic/Shopping_Cart.py
@@ -1,47 +1,65 @@
-# Creates a customer for the database to store
+""""Use django.contrib.auth to authorize a customer"""
+
+"""
+
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "F_J_app.settings")
+"""
+
 import os
 import sys
+from django.conf import settings
+from django.core.wsgi import get_wsgi_application
+
+# Get the directory of the current script
+script_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Append the parent directory to sys.path
+sys.path.append(os.path.dirname(script_dir))
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "F_J_app.settings")
+application = get_wsgi_application()
+# Creates a customer for the database to store
+
 
 # Get the directory of the current script
 script_dir = os.path.dirname(os.path.abspath(__file__))
 from django.contrib import messages
+from django.db import models
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect, get_object_or_404
-from Database.models import Cart
+from Database.models import Cart, Product, cartItem
 
 # Append the parent directory to sys.path
 sys.path.append(os.path.dirname(script_dir))
 
-from django.contrib import messages
-from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, redirect, get_object_or_404
-from Database.models import Cart
-
 class Cart_Shop:
+
+    def productlist(request):
+        productlist = Product.objects.all()
+        return render(request, "product.html", {"products":productlist})
+    
     @login_required
-    def cart_view(request):
-        cartlist = Cart.objects.filter(customer = request.customer)
-        totalprice = sum(citem.product.product_cost * citem.quantity for citem in cartlist)
+    def viewCart(request):
+        cart= request.customer.cart
+        cartlist = cartItem.objects.filter(cart = cart)
+        totalprice = sum(citem.product.cost * citem.quantity for citem in cartlist)
         holder = {"cart items": cartlist, "totalprice": totalprice }
         return render(request, "cart/shop_cart.html", holder)
+    
     @login_required
-    def add_product_to_cart(request, product_name):
-        cartlist = Cart.objects.filter(customer = request.customer, product=product_name)
-        if cartlist:
+    def add_product_to_cart(request, productid):
+        product = Product.objects.get(pid = productid)
+        cart, created = Cart.objects.get_or_create(customer = request.customer)
+        cartlist, pitem = cartItem.objects.get_or_create(cart=cart, product=product)
+        if not pitem:
             cartlist.quantity+=1
             cartlist.save()
             messages.success(request, "This product is added to your cart")
-        else:
-            Cart.objects.create(customer = request.customer, product=product_name)
-            messages.success(request, "This product is added to your cart")
-        return redirect("cart:cart_view")
+        return redirect("productlist")
     @login_required
-    def remove_product_from_cart(request, item_id):
-        cartlist = get_object_or_404(Cart, id = item_id)
+    def remove_product_from_cart(request, productid):
+        cartlist = get_object_or_404(Cart, id = productid)
     
         if cartlist.customer == request.customer:
             cartlist.delete()
             messages.success(request, "This product has been removed")
-        return redirect("cart:cart_view")
-
-   
+        return redirect("cart:viewCart")
